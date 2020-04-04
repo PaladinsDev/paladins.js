967f2c206122d8c01c3b4044f2ed20a1003b2f1c
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-04 19:13:43 -0400

    Adding service url.

--- src/api.ts
+++ src/api.ts
@@ -1,3 +1,7 @@
 export default class API {
-    
+    private serviceUrl: string = 'http://api.paladins.com/paladinsapi.svc';
+
+    public getServiceUrl(): string {
+        return this.serviceUrl;
+    }
 }
