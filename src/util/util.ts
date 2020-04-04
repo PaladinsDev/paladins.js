2244ece013b721668e83b2d6b96b122fda721d48
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-04 19:41:02 -0400

    Adding util class with mergeDefaults method.

---
+++ src/util/util.ts
@@ -0,0 +1,19 @@
+const has = (o: any, k: any) => Object.prototype.hasOwnProperty.call(o, k);
+
+export default class Util {
+    public static mergeDefaults(defaultOptions: { [key: string]: any}, givenOptions: { [key: string]: any}): { [key: string]: any} {
+        if (!givenOptions) {
+            return defaultOptions;
+        }
+
+        for (let key in defaultOptions) {
+            if (!has(givenOptions, key) || givenOptions[key] == undefined) {
+                givenOptions[key] = defaultOptions[key]
+            } else if (givenOptions[key] === Object(givenOptions[key])) {
+                givenOptions[key] = Util.mergeDefaults(defaultOptions[key], givenOptions[key]);
+            }
+        }
+        
+        return givenOptions;
+    }
+}
