5b7dd616c744e8643586f71afffb4f0d631992fb
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-06 10:20:37 -0400

    Adding constructor with options and merging.

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,12 @@
+import Util from "./util/util";
+import { DefaultOptions } from "./util/constants";
+
 export default class API {
     private serviceUrl: string = 'http://api.paladins.com/paladinsapi.svc';
+
+    constructor(private options = {}) {
+        this.options = Util.mergeDefaults(DefaultOptions, options);
+    }
 
     public getServiceUrl(): string {
         return this.serviceUrl;
