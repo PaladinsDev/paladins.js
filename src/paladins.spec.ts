43db508a9d93a8861659c2d4c351df2fd376af40
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-06 17:19:18 -0400

    Adding more to the test to make it functional.

--- src/paladins.spec.ts
+++ src/paladins.spec.ts
@@ -1,7 +1,20 @@
 import API from './paladins';
+import { PrivateProfileError } from './errors';
 
-let api = new API();
+let api = new API({
+    devId: process.env.DEV_ID,
+    authKey: process.env.AUTH_KEY
+});
 
 test('api service url should be present and set', () => {
     expect(api.getServiceUrl()).toBe('http://api.paladins.com/paladinsapi.svc');
 });
+
+test('player profile should throw private profile error', async () => {
+    try {
+        await api.getPlayer(15321771);
+        expect(true).toBe(false)
+    } catch (err) {
+        expect(err.name).toBe(PrivateProfileError.name);
+    }
+})
