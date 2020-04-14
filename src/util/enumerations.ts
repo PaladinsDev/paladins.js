99675c658b459cfbd9a0a53036828ef762f84396
Matthew Hatcher <matthewh@halfpetal.com>
2020-04-14 05:38:04 -0400

    Adding enums for developer ease.

---
+++ src/util/enumerations.ts
@@ -0,0 +1,79 @@
+export enum Portals {
+    Unknown = 0,
+    HiRez = 1,
+    Steam = 5,
+    Epic = 28,
+    PSN = 9,
+    XboxLive = 10,
+    Nintendo = 22,
+    Discord = 25,
+    Mixer = 14,
+    Facebook = 12,
+    Google = 13
+}
+
+export enum Languages {
+    English = 1,
+    German = 2,
+    French = 3,
+    Chinese = 5,
+    Spanish = 9,
+    Portuguese = 10,
+    Russian = 11,
+    Polish = 12,
+    Turkish = 13
+}
+
+export enum Ranks {
+    Qualifying = 0,
+    Bronze_V = 1,
+    Bronze_IV = 2,
+    Bronze_III = 3,
+    Bronze_II = 4,
+    Bronze_I = 5,
+    Silver_V = 6,
+    Silver_IV = 7,
+    Silver_III = 8,
+    Silver_II = 9,
+    Silver_I = 10,
+    Gold_V = 11,
+    Gold_IV = 12,
+    Gold_III = 13,
+    Gold_II = 14,
+    Gold_I = 15,
+    Platinum_V = 16,
+    Platinum_IV = 17,
+    Platinum_III = 18,
+    Platinum_II = 19,
+    Platinum_I = 20,
+    Diamond_V = 21,
+    Diamond_IV = 22,
+    Diamond_III = 23,
+    Diamond_II = 24,
+    Diamond_I = 25,
+    Master = 26,
+    Grandmaster = 27
+}
+
+export enum ProfileStates {
+    Offline = 0,
+    In_Lobby = 1,
+    Champion_Selection = 2,
+    In_Match = 3,
+    Online = 4,
+    Unknown = 5
+}
+
+export enum Queue {
+    Unknown = 0,
+    Casual_Siege = 424,
+    Team_Deathmatch = 469,
+    Onslaught = 452,
+    Ranked_Keyboard = 486,
+    Ranked_Controller = 428,
+    Shooting_Range = 434,
+    Training_Siege = 425,
+    Training_Team_Deathmatch = 470,
+    Training_Onslaught = 453,
+    Test_Maps = 445
+}
