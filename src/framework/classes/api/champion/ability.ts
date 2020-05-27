fdaed1f7db1477cc95c6b169e00f7cce176b11b8
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-27 15:29:25 -0400

    Adding base ability.

---
+++ src/framework/classes/api/champion/ability.ts
@@ -0,0 +1,35 @@
+import { DamageType } from './enumerations';
+
+export default class Ability {
+    private _name: string;
+    private _id: number;
+    private _description: string;
+    private _iconURL: string;
+    private _damageType: DamageType;
+    private _rechargeSeconds: number;
+
+    constructor(/** @ignore */private data: {[key:string]:any}) {
+        this._name = data['Summary'];
+        this._id = data['Id'];
+        this._description = data['Description'];
+        this._iconURL = data['URL'];
+        this._rechargeSeconds = data['rechargeSeconds'];
+
+        switch (data['damageType']) {
+            case 'AoE': 
+                this._damageType = DamageType.AOE;
+                break;
+            case 'Direct': 
+                this._damageType = DamageType.DIRECT;
+                break;
+            case 'Physical': 
+                this._damageType = DamageType.PHYSICAL;
+                break;
+            case 'True': 
+                this._damageType = DamageType.TRUE;
+                break;
+            default:
+                this._damageType = DamageType.UNKNOWN;
+        }
+    }
+}
