388eddd49846da6464b96ba89ff920adc685ca9a
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-27 15:29:03 -0400

    Adding enums

---
+++ src/framework/classes/api/champion/enumerations.ts
@@ -0,0 +1,15 @@
+export enum DamageType {
+    AOE,
+    DIRECT,
+    PHYSICAL,
+    TRUE,
+    UNKNOWN,
+}
+
+export enum Role {
+    DAMAGE,
+    FLANK,
+    FRONT_LINE,
+    SUPPORT,
+    UNKNOWN,
+}
