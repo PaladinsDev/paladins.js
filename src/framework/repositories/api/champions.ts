a8e53e32cabb81828707386a49bb5712ead65fb6
Matthew Hatcher <matthewh@halfpetal.com>
2020-05-27 16:04:32 -0400

    Adding champions repository.

---
+++ src/framework/repositories/api/champions.ts
@@ -0,0 +1,14 @@
+import { Repository } from '../repository';
+import Champion from '../../classes/api/champion/champion';
+
+export default class ChampionsRepository implements Repository {
+    constructor(private _championCollection: Champion[]) {}
+
+    public all(): Champion[] {
+        return this._championCollection;
+    }
+
+    public find(needle: any, key?: any): any {
+
+    }
+}
