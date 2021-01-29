ad37b0ace6f26b72e07180218820dedf8233830d
Matthew Hatcher <matthewh@halfpetal.com>
2021-01-29 13:59:48 -0500

    Writing to text file.

---
+++ tools/export-champions-for-enum.ts
@@ -0,0 +1,17 @@
+import { API } from '../src/api';
+import fs from 'fs';
+
+let api = new API({
+    devId: process.env.DEV_ID,
+    authKey: process.env.AUTH_KEY
+});
+
+api.getChampions().then((data: any) => {
+    let champs = '';
+    
+    data.forEach((champ: any) => {
+        champs += `${champ.Name}: ${champ.id}\n`;
+    });
+    
+    fs.writeFileSync('./tools/data/export-champions-for-enum.txt', champs);
+})
--- tools/export-champions.ts
+++
@@ -1,6 +0,0 @@
-import { API } from '../src/api';
-
-let api = new API({
-    devId: process.env.DEV_ID,
-    authKey: process.env.AUTH_KEY
-});
